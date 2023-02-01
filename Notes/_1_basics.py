1577cc898751f722359b4a9f60f971d1f91340cb
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-02-02 00:13:23 +0530

    added first set of notes

---
+++ Notes/_1_basics.py
@@ -0,0 +1,54 @@
+# # basic print function
+
+# print('hello world')
+
+# # second print function will print on new line
+
+# print('hi my age is ', 20, 'hey') # use comma to concatenate strings
+
+# #variables in python
+
+# name = 'Sangeet Sharma'
+
+# print('My name is ', name)
+
+# age = 20 #for integer we don't use quotes
+
+# # for concatenating strings we can use both addition sign and comma but to add integers and strings together we can only use comma
+
+
+
+# # string in python
+
+
+
+# #simply use \n for a new line
+
+# print('line 1 \n line 2')
+
+# #to print ' use \'
+
+# #special functions in python
+ 
+# name = 'Sangeet Sharma'
+ 
+# print(name.upper()) #return string with all letters in upper case
+ 
+# print(name) #original string hasn't changed
+ 
+# print(name.isupper()) #returns if all the letters are upper cased or not
+ 
+# print(name.upper().isupper())
+ 
+# print('length of name is',len(name))
+ 
+# print('index number of S is',name.index('S')) #returns the first index of given character in the string
+ 
+# print(name.replace('Sa', 'T')) #replace all the first strings with the second one
+ 
+ 
+ 
+# numbers in python
+ 
+ 
+ 
